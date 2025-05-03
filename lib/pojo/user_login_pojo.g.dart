740efaa50cdc850b8ccdb38c10f658a96255a3b6
eemmmml <1667432039@qq.com>
2025-05-03 22:46:07 +0800

    实现了服务端同步的登录功能

---
+++ lib/pojo/user_login_pojo.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_login_pojo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserLoginPojo _$UserLoginPojoFromJson(Map<String, dynamic> json) =>
+    UserLoginPojo(
+      code: (json['code'] as num).toInt(),
+      msg: json['msg'] as String,
+      data: json['data'] as String,
+    );
+
+Map<String, dynamic> _$UserLoginPojoToJson(UserLoginPojo instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'msg': instance.msg,
+      'data': instance.data,
+    };
